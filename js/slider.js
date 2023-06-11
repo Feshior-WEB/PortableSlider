e3d6719dc4318e948b20aaf8f0dfaeaa02df45e9
Vladyslav <vladislavshvetsua@gmail.com>
2023-06-11 17:38:26 +0200

    Improved desing & bug fixing

--- js/slider.js
+++ js/slider.js
@@ -9,51 +9,55 @@ let isDragging = false,
     animationId = 0, //id to cancel the request frame
     currentIndex = 0, //Current slide
     prevIndex = -1,
-    cardWidth = 0,
-    screenWidth = 0,
-    maxCardsOnScreen = 0;
+    cardWidth = 0;
 
 //Use slider only on mobile devices
-if (window.innerWidth < 600) {
-    slides.forEach((slide, index) => {
-        //Removign drag image effect
-        const slideImage = slide.querySelector('img');
-        slideImage.addEventListener('dragstart', (e) => e.preventDefault());
+function initializeSlider() {
+    if (window.innerWidth < 600) {
+        document.querySelector('.progressbar-container').style.display =
+            'block';
+        slides.forEach((slide, index) => {
+            //Removign drag image effect
+            const slideImage = slide.querySelector('img');
+            slideImage.addEventListener('dragstart', (e) => e.preventDefault());
 
-        //Mouse events
-        slide.addEventListener('mousedown', (event) => {
-            touchStart(event, index);
-        });
-        slide.addEventListener('mouseup', (event) => {
-            touchEnd(event);
-        });
-        slide.addEventListener('mouseleave', (event) => {
-            touchEnd(event);
-        });
-        slide.addEventListener('mousemove', touchMove);
+            //Mouse events
+            slide.addEventListener('mousedown', (event) => {
+                touchStart(event, index);
+            });
+            slide.addEventListener('mouseup', (event) => {
+                touchEnd(event);
+            });
+            slide.addEventListener('mouseleave', (event) => {
+                touchEnd(event);
+            });
+            slide.addEventListener('mousemove', touchMove);
 
-        // //Touch events
-        slide.addEventListener('touchstart', (event) => {
-            touchStart(event, index);
+            //Touch events for mobile devivces
+            slide.addEventListener('touchstart', (event) => {
+                touchStart(event, index);
+            });
+            slide.addEventListener('touchend', (event) => {
+                touchEnd(event);
+            });
+            slide.addEventListener('touchmove', touchMove);
         });
-        slide.addEventListener('touchend', (event) => {
-            touchEnd(event);
-        });
-        slide.addEventListener('touchmove', touchMove);
-    });
-} else {
-    document.querySelector('.progressbar-container').style.display = 'none';
+    } else {
+        //Removing progressbar for PC version
+        document.querySelector('.progressbar-container').style.display = 'none';
+        //Reseting position
+        prevIndex = -1;
+        currentIndex = 0;
+        setPositionByIndex();
+    }
 }
-//Removing context menu
-window.oncontextmenu = function (event) {
-    event.preventDefault();
-    event.stopPropagation();
-    return false;
-};
+
 //Calculating card width
 recalculateSizes();
+initializeSlider();
 addEventListener('resize', (event) => {
     recalculateSizes();
+    initializeSlider();
 });
 
 function recalculateSizes() {
@@ -61,69 +65,57 @@ function recalculateSizes() {
         let style = window.getComputedStyle(slides[0]);
         cardWidth = slides[0].offsetWidth + parseFloat(style.marginLeft);
     }
-    screenWidth = window.innerWidth;
-    maxCardsOnScreen = Math.trunc(screenWidth / cardWidth);
 }
 
 function touchStart(event, index) {
-    //if(slides.length - 1 !== index)
     currentIndex = index;
-
     startPosition = getPositionX(event);
-
     isDragging = true;
-
     animationId = requestAnimationFrame(animation);
-    slider.classList.add('grabbing');
 }
 
 function touchEnd(event) {
     isDragging = false;
     cancelAnimationFrame(animationId);
-
     const moveBy = currentTranslate - prevTranslate;
 
-    //For mobile devices
-    if (!event.type.includes('mouse')) {
-        //If moving slider right
-        if (moveBy < -7) {
-            //Preventing the leaving out of range
-            if (currentIndex + 1 === slides.length) {
-                currentIndex -= 1;
-            } else if (currentIndex - prevIndex > 1) {
-                //Clicked on the secound item
-                //No need in increasing currentIndex, because user has already clicked on
-                //the secound items, so index will already corresponds to it
-                prevIndex++;
-            }
-            //If clicked on first item; & preventing from slider moving out of range
-            else if (currentIndex < slides.length - 2) {
-                prevIndex = currentIndex;
-                currentIndex += 1;
-            }
+    //If moving slider right
+    if (moveBy < -7) {
+        //Preventing the leaving out of range
+        if (currentIndex + 1 === slides.length) {
+            currentIndex -= 1;
+        } else if (currentIndex - prevIndex > 1) {
+            //Clicked on the secound item
+            //No need in increasing currentIndex, because user has already clicked on
+            //the secound items, so index will already corresponds to it
+            prevIndex++;
         }
-
-        //If moving slider left
-        if (moveBy > 7) {
-            //If clicked on the secound item
-            if (currentIndex - prevIndex > 1) {
-                if (currentIndex - 1 > 0) {
-                    currentIndex -= 2;
-                    prevIndex = currentIndex - 1;
-                }
-            } else if (currentIndex - 1 >= 0) {
-                //If clicked on the first item
-                currentIndex -= 1;
-                prevIndex = currentIndex - 1;
-            }
-        }
-        //Prevent index change if user clicks on a item, instead of sliding it
-        if (Math.abs(moveBy) > 6) {
-            setPositionByIndex();
+        //If clicked on the first item; & preventing from slider moving out of range
+        else if (currentIndex < slides.length - 2) {
+            prevIndex = currentIndex;
+            currentIndex += 1;
         }
     }
-    slider.classList.remove('grabbing');
-    setSliderByIndex();
+
+    //If moving slider left
+    if (moveBy > 7) {
+        //If clicked on the secound item
+        if (currentIndex - prevIndex > 1) {
+            if (currentIndex - 1 > 0) {
+                currentIndex -= 2;
+                prevIndex = currentIndex - 1;
+            }
+        } else if (currentIndex - 1 >= 0) {
+            //If clicked on the first item
+            currentIndex -= 1;
+            prevIndex = currentIndex - 1;
+        }
+    }
+    //Prevent index change if user clicks on a item, instead of sliding it
+    if (Math.abs(moveBy) > 6) {
+        setPositionByIndex();
+        setSliderByIndex();
+    }
 }
 
 function touchMove(event) {
